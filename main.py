457b58478b7256504170e94d93d642373a61a7e4
Pooja <malikpooja05121999@gmail.com>
2024-06-30 09:11:59 +0530

    model_trainer added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ml_project import logger
 from src.ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.ml_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from src.ml_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-
+from src.ml_project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -37,4 +37,13 @@ except Exception as e:
         raise e
 
 
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
